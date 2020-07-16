b0a4db5533fcee0ff48dc17165bd98e79504b422
umeshbhat <ubhat@mscripts.com>
2020-07-16 09:22:43 +0530

    added umeshbhat in test file

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -33,7 +33,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "World, umeshbhat was here";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
